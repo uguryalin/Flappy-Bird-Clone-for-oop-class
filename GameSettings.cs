191eb514535ed0e3f5dcfbe85ed07601bb5444a7
UĞUR <yalinugur00@outlook.com>
2026-03-04 02:01:04 +0300

    Add GameSettings class with game constants

---
+++ GameSettings.cs
@@ -0,0 +1,11 @@
+namespace FlappyBirdClone
+{
+    public static class GameSettings
+    {
+        public const int Gravity = 12;
+        public const int JumpStrength = -12;
+        public const int PipeSpeed = 8;
+        public const int PipeStartPos = 800;
+        public const int GameInterval = 20; // ms
+    }
+}
